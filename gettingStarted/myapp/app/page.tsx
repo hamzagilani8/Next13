290b4df1d321e819fc3e38924096b78d19a4ce5b
hamzagilani8 <65051395+hamzagilani8@users.noreply.github.com>
2023-01-07 15:01:33 +0500

    Newapp

---
+++ gettingStarted/myapp/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Hello world!!</h2>
+      <h2>hi!!</h2>
+    </div>
+  )
+}
